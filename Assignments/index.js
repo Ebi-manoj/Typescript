150e2e8b7cbd65da8af100e97fc6511e293ffe5f
Ebi manoj <ebimanoj28@gmail.com>
2025-07-03 15:41:51 +0530

    14

---
+++ Assignments/index.js
@@ -0,0 +1,189 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// 1. Accept a char input from the user and display it on the console.
+function question1() {
+    var char = prompt('Enter a Number');
+    if (char)
+        console.log("You entered ".concat(char));
+    else
+        prompt('You must enter a Number');
+}
+// question1();
+// 2. Accept two inputs from the user and output their sum.
+var Sum = /** @class */ (function () {
+    function Sum(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        console.log(num1 + num2);
+    }
+    return Sum;
+}());
+// new Sum(2, 4);
+//	3. Write a program to find the simple interest.
+function simpleInterest(p, i, y) {
+    var simpleInterest = (p * i * y) / 100;
+    return simpleInterest;
+}
+// console.log(simpleInterest(5, 3, 5));
+// 4. Write a program to check whether a student has passed or failed in a subject after he
+// or she enters their mark (pass mark for a subject is 50 out of 100).
+function passedOrNot(mark) {
+    if (mark < 50)
+        return 'Failed';
+    else
+        return 'Passed';
+}
+// 5. Write a program to show the grade obtained by a student after he/she enters their total mark percentage.
+function checkGrade(mark) {
+    if (mark > 100)
+        return 'Invalid Mark';
+    else if (mark >= 90)
+        return 'You got A';
+    else if (mark >= 80)
+        return 'You got B';
+    else
+        return 'You are average';
+}
+// 6. Using the ‘switch case’ write a program to accept an input number from the user and output the day as follows.
+function outputDay(num) {
+    var day;
+    switch (num) {
+        case 1:
+            day = 'Sunday';
+            break;
+        case 2:
+            day = 'Monday';
+            break;
+        default:
+            day = 'Invalid';
+    }
+    return day;
+}
+// 7. Write a program to print the multiplication table of given numbers.
+function multiplication(num) {
+    for (var i = 1; i <= 10; i++) {
+        console.log("".concat(i, " * ").concat(num, " = ").concat(i * num));
+    }
+}
+// multiplication(5);
+// 8. Write a program to find the sum of all the odd numbers for a given limit
+function sumOfoddNumber(limit) {
+    var sum = 0;
+    for (var i = 0; i <= limit; i++) {
+        if (i % 2 == 1)
+            sum += i;
+    }
+    return sum;
+}
+// // 9. Write a program to print the following pattern
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+function printPattern(num) {
+    var output = [];
+    for (var i = 1; i <= num; i++) {
+        for (var j = 1; j <= i; j++) {
+            output.push(j);
+        }
+        console.log.apply(console, output);
+        output = [];
+    }
+}
+// printPattern(5);
+// 10. Write a program to interchange the values of two arrays.
+function swapArray(arr, arr2) {
+    var temp = __spreadArray([], arr, true);
+    arr.length = 0;
+    arr.push.apply(arr, arr2);
+    arr2.length = 0;
+    arr2.push.apply(arr2, temp);
+}
+var arr = [1, 2, 3];
+var arr2 = [8, 9, 0];
+// 11. Write a program to find the number of even numbers in an array
+function findEven(arr) {
+    var count = 0;
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var n = arr_1[_i];
+        if (n % 2 == 0)
+            count++;
+    }
+    return count;
+}
+//12. Write a program to sort an array in descending order
+function sort(nums) {
+    if (nums.length <= 1) {
+        return nums;
+    }
+    var p = nums[nums.length - 1];
+    var left = [];
+    var right = [];
+    for (var i = 0; i < nums.length - 1; i++) {
+        if (nums[i] < p) {
+            left.push(nums[i]);
+        }
+        else {
+            right.push(nums[i]);
+        }
+    }
+    return __spreadArray(__spreadArray(__spreadArray([], sort(right), true), [p], false), sort(left), true);
+}
+// 13. Write a program to identify whether a string is a palindrome or not
+function checkPalindrome(str) {
+    var start = 0;
+    var end = str.length;
+    while (start < end) {
+        if (str[start] !== str[end])
+            return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+// console.log(checkPalindrome('malayalam'));
+// 14. Write a program to add to two dimensional arrays
+function add2DArray(arr1, arr2) {
+    var result = [];
+    for (var i = 0; i < arr1.length; i++) {
+        var sum = [];
+        for (var j = 0; j < arr1[i].length; j++) {
+            sum[j] = arr1[i][j] + arr2[i][j];
+        }
+        result.push(sum);
+    }
+    return result;
+}
+// const arr1 = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// const arr22 = [
+//   [10, 20, 30],
+//   [40, 50, 60],
+//   [70, 80, 90],
+// ];
+// console.log(add2DArray(arr1, arr22));
+// 15. Write a program to accept an array and display it on the console using functionss
+function mainFor15(get, display) {
+    var nums = get();
+    display(nums);
+}
+function getArray() {
+    var input = prompt('Enter number');
+    var nums = input.split(' ').map(function (num) { return +num; });
+    return nums;
+}
+function displayArray(nums) {
+    console.log.apply(console, nums);
+}
+// mainFor15(getArray, displayArray);
